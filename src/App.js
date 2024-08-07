c090944ea0075ca652a6225857537c5fa839cfd1
Hrishikesh Joshi <Hrishi@idealoftstudio.com>
2024-08-07 13:08:10 +0530

    first trial commit to the ui branch in git

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
   const value = "World";
-  return <div>This is an update Hello {value}</div>;
+  return <div>This is an update Hello {value} This is ui build</div>;
 }
 
 export default App;
